b8fdf7fd0db45fcf521d8d9cc29ebdff870f1d83
Lisa <tranlisa@live.de>
2022-08-09 10:16:00 +0200

    change dir

---
+++ src/main/java/main/Point.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Point {
+    int n;
+    int result;
+
+    public Point(int n, int result) {
+        this.n = n;
+        this.result = result;
+    }
+
+    public int getn() {
+        return n;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
---
+++ src/main/java/main/Start.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Start {
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+        System.out.println("Which ");
+
+    }
+}
