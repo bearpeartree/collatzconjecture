1e6c91985cdc46c23582dfe7250da96348973936
Lisa <tranlisa@live.de>
2022-08-09 11:40:46 +0200

    add user-input

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -1,9 +1,26 @@
 package main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args) {
-        int number = Integer.parseInt(args[0]);
-        System.out.println("Which ");
+        String option = args[0];
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Give me a positive number!");
+        try {
+            while(sc.hasNext()) {
+                int number = sc.nextInt();
+                if(number < 0) {
+                    System.out.println("number should be positiv!");
+                }
+            }
+        } catch (InputMismatchException n) {
+            System.out.println("Not a number, try again later");
+        }
     }
+
 }
+
