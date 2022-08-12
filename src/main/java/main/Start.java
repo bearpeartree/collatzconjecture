6dc5421bd4d8031e61704c4bbb74fdf86ce91814
Lisa <tranlisa@live.de>
2022-08-12 12:18:03 +0200

    add computational sequence

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -1,24 +1,38 @@
 package main;
 
+import modus.Sequence;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Start {
+
+    static int startNumber;
     public static void main(String[] args) {
         String option = args[0];
 
-        Scanner sc = new Scanner(System.in);
+        Scanner stdIn = new Scanner(System.in);
 
         System.out.println("Give me a positive number!");
         try {
-            while(sc.hasNext()) {
-                int number = sc.nextInt();
+            while(stdIn.hasNext()) {
+                int number = stdIn.nextInt();
                 if(number < 0) {
                     System.out.println("number should be positiv!");
                 }
+                startNumber = number;
+                Sequence s = new Sequence();
+                s.printTestSequence(startNumber);
             }
         } catch (InputMismatchException n) {
             System.out.println("Not a number, try again later");
+        }
+
+    }
+
+    public void drawConjecture(String module) {
+        if(module.equals("punctual")) {
+            //Call mehthod punctual, single or multiple =>2 sequence
         }
     }
 
--- src/main/java/modus/Point.java
+++ src/main/java/modus/Point.java
@@ -1,4 +1,4 @@
-package main;
+package modus;
 
 public class Point {
     int n;
